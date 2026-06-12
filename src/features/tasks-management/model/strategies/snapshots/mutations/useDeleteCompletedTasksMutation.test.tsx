f5ef6b23b24d277fcde2091810412522fae1c4a4
justgoodman17 <justgoodman17@gmail.com>
2026-06-12 21:48:36 +0500

    refactor: correct test names

--- src/features/tasks-management/model/strategies/snapshots/mutations/useDeleteCompletedTasksMutation.test.tsx
+++ src/features/tasks-management/model/strategies/snapshots/mutations/useDeleteCompletedTasksMutation.test.tsx
@@ -90,7 +90,7 @@ describe("useDeleteCompletedTasksMutation Snapshots", () => {
     });
   });
 
-  it("roll backs deleted tasks in correct order when request fails", async () => {
+  it("rolls back deleted tasks in correct order when request fails", async () => {
     server.use(
       http.delete("*/tasks", async () => {
         await delay(100);
